b3e097314b2c990164e83af84e6b142f50493e3e
John Ho <denhackl@icqmail.com>
2013-05-24 16:23:22 +0200

    bugfixes + feature
    getAllDataFromUser gibt fix 10 parameter aus

--- cube_license_frontend/index_JOH.php
+++ cube_license_frontend/index_JOH.php
@@ -14,8 +14,20 @@
             
             create_license_key("blaise@pascal.de", "false");
 
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
             
-            echo license_exists("homo@fh-trier.de");
+            $data1 = getAllDataFromUser("blaise@pascal.de");
+            var_dump($data1);
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            $data2 = getAllDataFromUser("test2@best.de");
+            var_dump($data2);
+            
+            
+            //echo license_exists("blaise@pascal.de");
 
             
             

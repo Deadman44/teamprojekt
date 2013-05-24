4f351c042650d3887e84f023874eafc49f932290
Florian Schneider <schneif@hochschule-trier.de>
2013-05-24 15:05:21 +0200

    Sync-Fehlerkorrektur

--- cube_license_frontend/index.php
+++ cube_license_frontend/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -7,29 +6,6 @@
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />  
 </head>
-=======
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            include 'functions.php';
-            include 'dbtests.php';
-            
-
-            
-            $lkey = create_license_key("lolol@fh-trier.de");
-            echo " --> KEY <br>".$lkey."<br>";
-            check_license_key("lolol@fh-trier.de", "bratwurst", $lkey);
-            
-  
-        ?>
-    </body>
-</html>
->>>>>>> 3ee75d6a9704c9238bb4b61470eb28d1a09baf50
 
 <body>
 <div class="mySite">

7719e69b9fb0fdfd4cca9a3e26d84db68ca2733c
John Ho <denhackl@icqmail.com>
2013-06-02 10:57:27 +0200

    thread tests

---
+++ cube/src/check_methods.cpp
@@ -0,0 +1,17 @@
+#include <cube.h>
+#include <boost\thread.hpp>
+
+void permanent_check()
+{
+	boost::thread workerThread(worker);
+
+}
+
+void worker()
+{
+	conoutf("WORKING WORKING");
+	boost::posix_time::seconds waiting(10);
+	boost::this_thread::sleep(waiting);
+
+
+}

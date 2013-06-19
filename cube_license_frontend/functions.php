dec3068f32998df9c83052f199a3a5640fc38a2f
John Ho <denhackl@icqmail.com>
2013-06-19 15:32:41 +0200

    bfsg
    sgd

--- cube_license_frontend/functions.php
+++ cube_license_frontend/functions.php
@@ -1077,9 +1077,12 @@ function destroyStartBat(){
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if (is_readable("C:/w/cube/cube/start.bat"))
                 {
 =======
+=======
+>>>>>>> 43197eedf0683235da971b245b29989f046bbbf2
 =======
 >>>>>>> 43197eedf0683235da971b245b29989f046bbbf2
 =======

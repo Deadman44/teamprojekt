2fe1a8cb84d2fafa84728b3e714ee44c68c19c21
Florian Schneider <schneif@hochschule-trier.de>
2013-06-23 11:53:47 +0200

    Active-Feld regelmäßig auf 0 setzen

--- cube_license_frontend/readFile.php
+++ cube_license_frontend/readFile.php
@@ -5,4 +5,38 @@ function readFileData(){
 	//echo $datei;
 }
 
+function dropActive($Qemail,$ticket){
+	while(true){
+
+		$credentials = getCredentialsFromFile();
+		$credentialsArr = explode(":", $credentials);
+		$user = $credentialsArr[0];
+		$pwd = $credentialsArr[1];
+    
+		$active = "ERROR NOT FOUND";
+    
+		$mysqli = @new mysqli("127.0.0.1",$user,$pwd,"cube_license");
+		if($mysqli->connect_errno)
+		{
+				echo "FAIL";
+				return "SERVER_ERROR";
+		}
+		else
+		{
+
+            $query = "UPDATE USER set ACTIVE=0 where EMAIL=? AND TICKET=?";
+            $result = $mysqli->prepare($query);
+            $result->bind_param('ss',$Qemail,$ticket);
+            $result->execute();
+            $result->bind_result($active);
+            while($result->fetch())
+            {
+            }     
+		}
+		$mysqli->close();   
+
+		sleep(60*5);	// 300 Sekunden warten
+	}
+
+}
 ?>
